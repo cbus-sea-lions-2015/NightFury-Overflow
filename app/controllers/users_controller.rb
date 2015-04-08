d59c1a60cdacdcda4453388930e9c81d10ed322d
Daniel Martinez <mikedmtz@gmail.com>
2015-04-08 18:27:47 -0400

    added signup page for skip auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate, only: [:index]
+  skip_before_action :authenticate, only: [:index, :new]
 
   def index
     @users = User.all
