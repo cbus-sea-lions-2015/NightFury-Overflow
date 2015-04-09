f0bf00c67b96ae1059492001eb9176693b6a8d64
Tirthankar Bhattacharjee <feignfroggie@gmail.com>
2015-04-09 13:32:22 -0400

    add show for question

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
 
-  describe "GET index" do
+  describe "GET #index" do
     it 'shows all quesitons, newest first' do
       5.times {Question.create!(title: "Why this title?" , body:"I want to know why this title.", user: User.first) }
       questions = Question.order(created_at: :desc)
@@ -10,7 +10,14 @@ RSpec.describe QuestionsController, type: :controller do
       expect(assigns(questions)).to eq(@questions)
     end
   end
-
+  
+  describe "GET #show" do
+    it 'displays the question' do
+      question = Question.create!(title: "Why this title?" , body:"I want to know why this title.", user: User.first)
+      get :show, {id: question.id}
+      expect(assigns(question)).to eq(@question)
+    end
+  end
 
 
 end
