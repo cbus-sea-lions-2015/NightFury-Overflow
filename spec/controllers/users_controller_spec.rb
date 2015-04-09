70d71297e9dc6ec70f86f30fd0d74dc55e3ceaed
Daniel Martinez <mikedmtz@gmail.com>
2015-04-09 10:51:43 -0400

    add controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,5 +9,26 @@ describe UsersController do
       expect(assigns(User.all)).to eq(@users)
     end
   end
-  
+
+  describe "GET #show" do
+    it "assigns the requested user as @user" do
+      get :show, { user_id: user.to_param }
+      expect(assigns(:user)).to eq(user)
+    end
+  end
+
+  describe "GET #new" do
+    it "gets a new user" do
+      get :new
+      expect(user).to be_a User
+    end
+  end
+
+  describe "POST create" do
+    context "when valid params are passed" do
+      it "creates a new User" do
+        expect{post :create, user: { email: 'dan@dan1.com', password: "1234" }}.to change{User.count}.by(1)
+      end
+    end
+  end
 end
