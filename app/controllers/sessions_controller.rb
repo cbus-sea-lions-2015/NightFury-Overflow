745a1d4a7b28abcf2cefbc936df0897e78832e7b
Daniel Martinez <mikedmtz@gmail.com>
2015-04-09 12:31:42 -0400

    revised user routes, added spec

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to profile_path(@user)
+      redirect_to profile_path
     else
       render :new, alert: "Incorrect username or password."
     end
