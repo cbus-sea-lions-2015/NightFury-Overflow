0e51b34ff8131d2af6a2e6955e614d05a7c59221
Jan Dennison <jldennison@gmail.com>
2015-04-09 16:42:52 -0400

    worked out delete path by adding @commentable to controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,30 @@
 class CommentsController < ApplicationController
+  before_action :set_commentable
 
   def create
-    @comment = Comment.new(comment_params)
+    @comment = current_user.comments.new(comment_params.merge(commentable: @commentable))
     if @comment.save
-      render :_show
+      redirect_to @commentable, notice: 'Comment saved.'
     else
-      render :new
+      redirect_to @commentable, notice: 'Comment not saved, try again.'
     end
   end
 
   def destroy
-    comment = Comment.find(params[:comment].id)
+    comment = current_user.comments.find_by(id: params[:id], commentable: @commentable)
     comment.destroy
     redirect_to root_path
   end
 
   private
+
+  def set_commentable
+    @commentable = if params[:answer_id]
+      Answer.find_by(id: params[:answer_id], question_id: params[:question_id])
+    else
+      Question.find_by(id: params[:question_id])
+    end
+  end
 
   def comment_params
     params.require(:comment).permit(:body, :user, :commentable)
