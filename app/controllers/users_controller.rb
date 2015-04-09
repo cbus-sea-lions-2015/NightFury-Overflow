6ea0251b0ca28422e90d5b5a2f38ce90428e93c2
Daniel Martinez <mikedmtz@gmail.com>
2015-04-09 10:52:20 -0400

    add account creation notice

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,10 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.create(user_params)
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to profile_path, notice: 'Account was successfully created.'
     else
       render :new
     end
