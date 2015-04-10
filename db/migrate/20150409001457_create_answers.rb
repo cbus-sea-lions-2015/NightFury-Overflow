653d62f311dbd1d65b15cf860632cbfd5545c67f
Jan Dennison <jldennison@gmail.com>
2015-04-10 17:17:05 -0400

    Revert "Remove empty files"

--- db/migrate/20150409001457_create_answers.rb
+++ db/migrate/20150409001457_create_answers.rb
@@ -2,7 +2,6 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.string :body
-      t.boolean :best
       t.references :user, index: true, foreign_key: true
       t.references :question, index: true, foreign_key: true
 
