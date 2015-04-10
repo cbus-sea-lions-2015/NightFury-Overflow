0fc34a5ef0d3e2b1956771f906d9f35edb2614da
Daniel Martinez <mikedmtz@gmail.com>
2015-04-10 15:35:56 -0400

    merged conflicts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,28 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function(){
+  // URL Param Function shim http://stackoverflow.com/questions/19491336/get-url-parameter-jquery
+  function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+      var sParameterName = sURLVariables[i].split('=');
+      if (sParameterName[0] == sParam) {
+        return sParameterName[1];
+      }
+    }
+  }
+
+  // Update active button for sort order on index page
+  var sort_order = getUrlParameter('sort_order') || '',
+      parent = $('.question-sort');
+  if (sort_order == 'voted') {
+    parent.find('.active').removeClass('active');
+    parent.find('.top-voted').addClass('active');
+  } else if (sort_order == 'commented') {
+    parent.find('.active').removeClass('active');
+    parent.find('.trending').addClass('active');
+  };
+});
