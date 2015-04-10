d49dbd14b94169b19c22b9ed4553f97645b4201d
Daniel Martinez <mikedmtz@gmail.com>
2015-04-10 16:50:12 -0400

    add answer sort logic

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,13 +2,14 @@ class Question < ActiveRecord::Base
   scope :vote_order, ->() { joins(:votes).group("questions.id").order("sum(votes.direction) DESC") }
   scope :comment_order, ->() { joins(:comments).group("questions.id").order("count(comments) DESC") }
 
-  scope :vote_order, ->() { joins(:votes).group("questions.id").order("sum(votes.direction) DESC") }
-  scope :comment_order, ->() { joins(:comments).group("questions.id").order("count(comments) DESC") }
-
   belongs_to :user
   has_many :responses, class_name: 'Answer'
   has_many :answers
   has_many :comments, as: :commentable
   has_many :votes, as: :votable
+
+  def sort_answers
+    self.answers.joins(:votes).group('answers.id,best').order('best DESC, sum(votes.direction) DESC ')
+  end
 
 end
