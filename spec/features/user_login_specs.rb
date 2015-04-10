abd170541aba02d999d94f61f639258a63f63939
Tirthankar Bhattacharjee <feignfroggie@gmail.com>
2015-04-10 15:47:53 -0400

    edited tests for not logged in user

--- spec/features/user_login_specs.rb
+++ spec/features/user_login_specs.rb
@@ -4,29 +4,36 @@ require 'capybara/rspec'
 feature "User visits page without login in" do
   before :each do
     visit '/logout'
-    visit '/'
   end
 
   scenario "when a user visits the home page the user can see brand and links" do
+    visit '/'
     expect(page).to have_content("NightFuryOverflow")
     expect(page).to have_link("Recent")
     expect(page).to have_link("Top Voted")
     expect(page).to have_link("Trending")
-    expect(page).to have_link("Login")
+    expect(page).to have_link("Log In")
     expect(page).to have_link("Sign Up")
   end
 
   scenario "when a user visits the home page the user can see all questions" do
-    questions = Questions.pluck(:title)
+    User.create!(email: "batman@batman.com", password:"batman")
+    5.times {Question.create!(title: "Why this title?" , body:"I want to know why this title.", user: User.first) }
+    visit '/'
+    questions = Question.pluck(:title)
     questions.each do |question|
       expect(page).to have_link(question)
     end
   end
 
   scenario "when a user click on the question, can see the answers" do
-    # question = Question.first
-    # click_link(question.title)
-    # expect(page).to redirect_to(question)
+    User.create!(email: "batman@batman.com", password:"batman")
+    Question.create!(title: "Why this title?" , body:"I want to know why this title.", user: User.first)
+    question = Question.first
+    visit '/'
+    click_link(question.title)
+    expect(page.current_path).to eq("/questions/#{question.id}")
+    expect(page).to have_content("Answers")
   end
 
 end
