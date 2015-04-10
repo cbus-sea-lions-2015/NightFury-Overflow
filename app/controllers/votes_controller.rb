0dc157593bd6b98909a7b89fcd33600a04f213b8
Daniel Martinez <mikedmtz@gmail.com>
2015-04-10 00:01:45 -0400

    added support for voting

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,5 @@
 class VotesController < ApplicationController
+  before_action :set_votable
 
   def create
     @vote = Vote.new(vote_params)
@@ -9,15 +10,29 @@ class VotesController < ApplicationController
     end
   end
 
+  def create
+    @vote = @votable.votes.new(vote_params.merge({user: current_user}))
+    if @vote.save
+      redirect_to @question
+    else
+     redirect_to @question, notice: 'Vote not saved, try again.'
+    end         
+  end       
+
   def destroy
-    vote = Vote.find(params[:vote].id)
+    vote = current_user.vote.find_by(id: params[:id])
     vote.destroy
     redirect_to root_path
   end
 
   private
+  def set_votable
+    @question = Question.find_by(id: params[:question_id])
+    @answer = Answer.find_by(id: params[:answer_id]) if params[:answer_id]
+    @votable = @answer || @question
+  end
 
   def vote_params
-    params.require(:vote).permit(:user, :votable)
+    params.require(:vote).permit(:direction)
   end
 end
