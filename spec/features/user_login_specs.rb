4efc7e3dcffdab1212afe4e7bf6da7bf1d232eb8
Tirthankar Bhattacharjee <feignfroggie@gmail.com>
2015-04-10 15:20:57 -0400

    add more feature specs for homepage

--- spec/features/user_login_specs.rb
+++ spec/features/user_login_specs.rb
@@ -2,8 +2,31 @@ require 'rails_helper'
 require 'capybara/rspec'
 
 feature "User visits page without login in" do
-  scenario "when user is not logged in and visits the root page" do
+  before :each do
+    visit '/logout'
     visit '/'
-     expect(page).to have_content("NightFuryOverflow")
   end
+
+  scenario "when a user visits the home page the user can see brand and links" do
+    expect(page).to have_content("NightFuryOverflow")
+    expect(page).to have_link("Recent")
+    expect(page).to have_link("Top Voted")
+    expect(page).to have_link("Trending")
+    expect(page).to have_link("Login")
+    expect(page).to have_link("Sign Up")
+  end
+
+  scenario "when a user visits the home page the user can see all questions" do
+    questions = Questions.pluck(:title)
+    questions.each do |question|
+      expect(page).to have_link(question)
+    end
+  end
+
+  scenario "when a user click on the question, can see the answers" do
+    # question = Question.first
+    # click_link(question.title)
+    # expect(page).to redirect_to(question)
+  end
+
 end
